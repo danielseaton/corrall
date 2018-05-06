1f78c047f1cf449455f26b53d4f1b54c12260d92
danielseaton <daniel.seaton87@gmail.com>
2018-05-06 15:22:08 +0100

    get_snp_genotypes - function to get phased genotypes of specific SNP

---
+++ vcf_utils.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import cyvcf2
+import pandas as pd
+
+def get_snp_genotypes(chromosome, position, samples=None):
+    '''Returns a pandas DataFrame of genotypes, along with phasing status, for a specific SNP.'''
+
+    vcf_file = '/hps/nobackup/hipsci/scratch/genotypes/imputed/REL-2018-01/Full_Filtered/hipsci.wec.gtarray.HumanCoreExome.imputed_phased.20180102.genotypes.chr.{chromosome}.norm.renamed.recode.vcf.gz'.format(chromosome=chromosome)
+
+    if samples is None:
+        vcf = cyvcf2.VCF(vcf_file)
+        samples = vcf.samples
+    else:
+        vcf = cyvcf2.VCF(vcf_file, samples=samples)
+    
+    query_string = '{chromosome}:{position}-{position}'.format(chromosome=chromosome, position=position)
+
+    variants = [x for x in vcf(query_string)]
+
+    if len(variants) > 1:
+        error_message = '''Input vcf file contains more than one SNP at position {chromosome}:{position}'''.format(chromosome=chromosome,
+                                                                                                               position=position)
+        raise(ValueError(error_message))
+    if len(variants) == 0:
+        error_message = '''Input vcf file has no SNP at position {chromosome}:{position}'''.format(chromosome=chromosome,
+                                                                                                   position=position)
+        raise(ValueError(error_message))
+    
+    var = variants[0]
+    
+    genotype_df = pd.DataFrame(index=samples, columns=['chrA','chrB','phased'], data=var.genotypes)
+
+    return genotype_df
+
+#samples = ['HPSI0516i-pebf_2', 'HPSI0516i-zujs_5', 'HPSI1116pf-peru']
+#df = get_snp_genotypes(1,714439,samples)
