d9b6dde77228858a1e0b7020569d5796dd934a80
danielseaton <daniel.seaton87@gmail.com>
2019-01-13 21:07:18 +0000

    Perform multiple tests and aggregate results in a dataframe.

--- test_ase_interactions.py
+++ test_ase_interactions.py
@@ -23,6 +23,7 @@ n_genes = 5
 ase_short_name = 'all_leads'
 n_index_cols = 2
 
+min_n_cells = 50
 
 working_dir = '/nfs/leia/research/stegle/dseaton/hipsci/singlecell_endodiff'
 
@@ -34,7 +35,6 @@ factor_file = working_dir + '/data/sce_merged_afterqc_filt_allexpts_pseudotimean
 outfile_pattern = working_dir + '/data/ase_env_interactions/pseudotimeandmodules.{test_type}.'+ase_short_name+'.tsv'
 metadata_file = working_dir + '/data/sce_merged_afterqc_filt_allexpts_metadata_20180618.tsv'
 
-qtl_list = [('ENSG00000233927', '19_8387207_G_A')]
 
 allelic_df = pd.read_csv(alleliccounts_file, sep='\t', index_col=list(range(n_index_cols)), nrows=n_genes)
 total_df = pd.read_csv(totalcounts_file, sep='\t', index_col=list(range(n_index_cols)), nrows=n_genes)
@@ -42,18 +42,13 @@ ase_df = pd.read_csv(allelicfractions_file, sep='\t', index_col=list(range(n_ind
 factor_df = pd.read_csv(factor_file, sep='\t', index_col=0)
 
 
+# qtl_list = [('ENSG00000233927', '19_8387207_G_A')]
+# allelic_df = allelic_df.loc[qtl_list, :]
+# total_df = total_df.loc[qtl_list, :]
+# ase_df = ase_df.loc[qtl_list, :]
 
-allelic_df = allelic_df.loc[qtl_list, :]
-total_df = total_df.loc[qtl_list, :]
-ase_df = ase_df.loc[qtl_list, :]
-
-allelic_df = allelic_df.dropna(axis=1)
-total_df = total_df.dropna(axis=1)
-ase_df = ase_df.dropna(axis=1)
-
-ase_idx = qtl_list[0]
-ase_ds = ase_df.loc[ase_idx, :].dropna()
-
+if qtl_list is None:
+    qtl_list = ase_df.index
 
 factor = 'pseudotime'
 covariate_factors = []
@@ -64,9 +59,21 @@ covariate_factors = []
 # factor = 'respiration'
 # covariate_factors = ['pseudotime']
 
-y_ds, candidate_ds, covariate_df = statistical_models.get_model_dataframes(ase_ds, factor_df, factor, covariate_factors)
+list_of_outputs = []
 
-output = statistical_models.test_limix_lmm(y_ds, candidate_ds, covariate_df)
+for ase_idx in qtl_list:
+    ase_ds = ase_df.loc[ase_idx, :].dropna()
 
-model = limix.qtl.st_scan(candidate_ds, y_ds, lik='normal', M=covariate_df.values, verbose=False)
-print(model.variant_pvalues)
+    if ase_ds.shape[0]< min_n_cells:
+        continue
+    
+    y_ds, candidate_ds, covariate_df = statistical_models.get_model_dataframes(ase_ds, factor_df, factor, covariate_factors)
+    
+    output_ds = statistical_models.test_limix_lmm(y_ds, candidate_ds, covariate_df)
+    output_ds.name = ase_idx
+
+    list_of_outputs.append(output_ds)
+
+output_df = pd.concat(list_of_outputs, axis=1).transpose()
+output_df['factor'] = factor
+output_df['covariates'] = ', '.join(covariate_factors)
