2d38fb655166d4b06321a0bae1ad1df59c558105
danielseaton <daniel.seaton87@gmail.com>
2019-04-23 09:15:43 +0100

    shell script to run pipeline for one donor

---
+++ run_complete_pipeline.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DONOR=$1
+
+#DONORMAPPINGFILE='/nfs/leia/research/stegle/dseaton/hipsci/singlecell_endodiff/data/sce_merged_qc_filt_mnn_correct_slalom_metadata_20180310.subset.tsv'
+#DONORMAPPINGFILE='/nfs/leia/research/stegle/dseaton/hipsci/singlecell_endodiff/data/sce_merged_afterqc_beforefilt_metadata_20180618.linemapping.reformatted.tsv'
+DONORMAPPINGFILE='/nfs/leia/research/stegle/dseaton/hipsci/singlecell_endodiff/data/sce_merged_afterqc_filt_allexpts_metadata_20180618.mapping_donors_to_ase_files.tsv'
+
+
+OUTDIR='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor'
+
+TOTAL='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor/'$DONOR'.ase.lowthresh.totalcount.tsv'
+ALT='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor/'$DONOR'.ase.lowthresh.altcount.tsv'
+SNPINFO='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor/'$DONOR'.ase.lowthresh.snp_info.tsv'
+
+TOTAL2='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor/'$DONOR'.ase.lowthresh.totalcount.phased.tsv'
+ALT2='/hps/nobackup/hipsci/scratch/singlecell_endodiff/data_processed/ase/ase_aggregated_by_donor/'$DONOR'.ase.lowthresh.chrBcount.phased.tsv'
+
+GENEBED='/nfs/leia/research/stegle/dseaton/genomes/hg19/annotation/Homo_sapiens.GRCh37.75.exons.genenames.bed'
+
+python aggregate_data_by_donor.py $DONOR $DONORMAPPINGFILE $OUTDIR
+source activate cyvcf2_env && python phase_ase_quantifications.py $ALT $TOTAL $SNPINFO 
+source activate py27 && python aggregate_ase_by_gene.py $ALT2 $TOTAL2 $SNPINFO $GENEBED genelevel
+
