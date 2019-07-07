16bed9ca70ff3d2ebbd4187eb4ac785f59a7ffb8
danielseaton <daniel.seaton87@gmail.com>
2019-07-07 16:20:29 +0100

    refactor cellsnp vcf reading as a function to be called

--- cellsnp_utils.py
+++ cellsnp_utils.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import allel
 
-vcf_fields = ['variants/REF', 'variants/FILTER_PASS', 'variants/POS', 'variants/ID', 'variants/ALT', 'variants/QUAL', 'samples', 'variants/CHROM', 'calldata/GT', 'calldata/DP', 'calldata/AD']
+def read_cellsnp_vcf(cellsnp_vcf_file):
+    #DP is depth in each sample (REF+ALT), AD is number of ALT counts
+    vcf_fields = ['variants/REF', 'variants/FILTER_PASS', 'variants/POS', 'variants/ID', 'variants/ALT', 'variants/QUAL', 'samples', 'variants/CHROM', 'calldata/GT', 'calldata/DP', 'calldata/AD']    
+    
+    vcf = allel.read_vcf(cellsnp_vcf_file, fields=vcf_fields)
+    
+    total_count_mat = vcf['calldata/DP']
+    total_count_mat[total_count_mat == -1] = 0
+    
+    alt_count_mat = vcf['calldata/AD']
+    alt_count_mat = alt_count_mat[:,:,0]
+    alt_count_mat[alt_count_mat == -1] = 0
 
-#DP is depth in each sample (REF+ALT), AD is number of ALT counts
+    variants = vcf['variants/ID']
+    samples = vcf['samples']
 
-vcf = allel.read_vcf('./tests/data/cellsnp_output_example.vcf', fields=vcf_fields)
+    total_count_df = pd.DataFrame(total_count_mat, index=variants, columns=samples)
+    alt_count_df = pd.DataFrame(alt_count_mat, index=variants, columns=samples)
 
-total_count_mat = vcf['calldata/DP']
-total_count_mat[total_count_mat == -1] = 0
+    return alt_count_df, total_count_df
 
-
-#this needs to be massively reshaped
-alt_count_mat = vcf['calldata/AD']
-alt_count_mat = alt_count_mat[:,:,0] # I think this is what I want
-#alt_count_mat = alt_count_mat[alt_count_mat != -1]
-
-alt_count_mat[alt_count_mat == -1] = 0
-
-#samples = vcf['samples']
